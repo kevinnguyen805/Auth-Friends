98fc13f2a6b54b35e76c06a391ea9f9c410f70d1
admin <admin@admins-MacBook-Pro.local>
2019-10-15 13:09:53 -0700

    initial commit

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import {BrowserROuter as Router, Route, Link, Switch} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
